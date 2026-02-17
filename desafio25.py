34d4dcf4a56a7e68fa41be28b07315addc1cb92a
Guilherme <guioliveiralucena@gmail.com>
2026-02-17 14:28:02 -0400

    Corrigido um erro de digitação na descrição do comando 'rfind'

--- desafio25.py
+++ desafio25.py
@@ -4,6 +4,6 @@ print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
 print('\nA letra "a" aparece {} vezes nessa palavra!'.format(pergunta.count('A')))
 # O comando 'find' procura a letra da esquerda pra direita e mostra a posição dela 
 print('A letra "a" apareceu pela primeira vez na posição {}!'.format(pergunta.find('A') + 1))
-# O comando 'rfind' procura a letra da direita pra esquerd 
+# O comando 'rfind' procura a letra da direita pra esquerda
 print('A letra "a" apareceu pela última vez na posição {}!\n'.format(pergunta.rfind('A') + 1))
 print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
