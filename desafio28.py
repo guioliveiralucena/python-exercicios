57d445ad07264655aee7af776296d69f1afa20fa
Guilherme <guioliveiralucena@gmail.com>
2026-02-17 18:46:43 -0400

    exercicio-d-python

---
+++ desafio28.py
@@ -0,0 +1,11 @@
+pergunta = int(input('Quantos km(quilômetros) você está dirigindo? '))
+
+if pergunta >= 80:
+    print('\n=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=')
+    print('\nVocê recebeu uma multa, por estar acima do limite de velocidade!')
+    print('\n=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=')
+
+else:
+    print('\n=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=')
+    print('\nMuito bem! Você não ultrapassou o limite de velocidade!')
+    print('\n=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=')
