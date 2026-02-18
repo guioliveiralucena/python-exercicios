8589f87cd050a72305c0c3dbad9b1a1266e6daa2
Guilherme <guioliveiralucena@gmail.com>
2026-02-17 20:14:27 -0400

    exercicio-py

--- desafio28.py
+++ desafio28.py
@@ -1,8 +1,13 @@
-pergunta = int(input('Quantos km(quilômetros) você está dirigindo? '))
+pergunta = float(input('Quantos km(quilômetros) você está dirigindo? '))
+limite_de_velocidade = 80
+valor_por_km = 7.0
 
-if pergunta >= 80:
+if pergunta >= limite_de_velocidade:
+    km_acima = pergunta - limite_de_velocidade
+    multa = km_acima * valor_por_km
     print('\n=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=')
-    print('\nVocê recebeu uma multa, por estar acima do limite de velocidade!')
+    print('\nVocê ultrapassou {} km acima do limite de velocidade!'.format(km_acima))
+    print('\nVocê terá que pagar uma multa de R$:{}!'.format(multa))
     print('\n=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=')
 
 else:
